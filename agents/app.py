e9ffc1b6f59504278fef95eda6d22f154e24d794
Shubham Saini <sainishubham9928@gmail.com>
2025-02-09 18:26:11 +0530

    done fixes and bugs

--- agents/app.py
+++ agents/app.py
@@ -6,28 +6,30 @@ from langchain_community.utilities.sql_database import SQLDatabase
 from langchain import hub
 from langgraph.prebuilt import create_react_agent
 from langchain.chat_models import init_chat_model
-import os
 from twilio.rest import Client
-from models import db, init_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db, init_app
+from models import db, init_app,User,Expense
+
 
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expenses.db"
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
-TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_ACCOUNT_SID = "AC69ee94d756ec419887e0130803887697"
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+print(TWILIO_PHONE_NUMBER)
+print(TWILIO_AUTH_TOKEN)
+print(TWILIO_ACCOUNT_SID)
 init_app(app)
 migrate = Migrate(app, db)
-twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 
 def create_agent():
     llm = init_chat_model("gemini-1.5-flash", model_provider="google_genai")
-    db = SQLDatabase.from_uri("sqlite:///expenses.db")
+    db = SQLDatabase.from_uri("sqlite:///instance/expenses.db")
     toolkit = SQLDatabaseToolkit(db=db, llm=llm)
     toolkit.get_tools()
 
@@ -40,11 +42,12 @@ def create_agent():
     return agent_executor
 
 
-def process_user_query(user_phone, user_query):
+def process_user_query(user_phone, user_query,categories):
     agent_executor = create_agent()
     example_query = f"""
     You are a expense tracker and manager bot which help users to track there expenses and manage it and provide a detailed breakdown of the total expenses incurred by the user.
     Can you provide a resolution to the following query for user with user_phone {user_phone}?
+    The user phone is in structure *whatsapp:+91XXXXXXXXXX*
     <User Query>:
     {user_query}
     </User Query>
@@ -59,28 +62,40 @@ def process_user_query(user_phone, user_query):
     last_response = ""
     for event in events:
         last_response = event["messages"][-1].content
+        event["messages"][-1].pretty_print()
+        
     return last_response
 
 
 @app.route("/whatsapp", methods=["POST"])
 def whatsapp():
-    parsed_msg = request.json
+    parsed_msg = request.form
     from_number = parsed_msg.get("From")
     incoming_msg = parsed_msg.get("Body", "").strip()
     if not incoming_msg:
         return jsonify({"message": "❌ Please provide a message."}, 400)
+    user = db.session.query(User).filter_by(user_phone=from_number).first()
+    categories = "["
+    if not user:
+       user = User(user_phone=from_number, limit_amount=1000)
+       db.session.add(user)
+       db.session.commit() 
+    expense_user = Expense.query.filter_by(user_id=user.id).all()
+    for expense in expense_user:
+        categories += expense.category + ","
+    categories += "]"
+    
 
-    response_message = process_user_query(from_number, incoming_msg)
-    twilio_client.messages.create(
-        body=response_message,
+    response_message = process_user_query(from_number, incoming_msg,categories=categories)
+    message = client.messages.create(
+     body=response_message,
         from_=TWILIO_PHONE_NUMBER,
-        to=from_number,
-    )
-    return "", 204
+        to=from_number)
+    return jsonify({"message": "✅ Message sent successfully."}, 200)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 # response_message = process_user_query(
 #     "9876543210", "Tell me expenses in groceries for this month"
 # )
--- agents/models.py
+++ agents/models.py
@@ -12,18 +12,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_phone = db.Column(db.String(20), unique=True, nullable=False)
     limit_amount = db.Column(db.Float, nullable=False)
-    expenses = db.relationship("Expense", backref="user", lazy=True)
 
     def __repr__(self):
         return f"<User {self.user_phone}, Limit: {self.limit_amount}>"
 
+
+def get_date_time():
+    return datetime.now()
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     category = db.Column(db.String(50), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(200), nullable=True)
-    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    date = db.Column(db.DateTime, default=get_date_time(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
